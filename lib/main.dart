f2114ed72e0bf7362102b77ccfe9a007498f2cdf
samcuxx <samuellove228@gmail.com>
2025-05-18 16:50:39 +0000

    Splash Screen added

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ import 'screens/class_dashboard_screen.dart';
 import 'screens/dashboard/dashboard_screen.dart';
 import 'models/user_model.dart';
 import 'utils/app_theme.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 // Replace these with your own Supabase project credentials
 // You can find these in your Supabase project settings > API
@@ -17,7 +18,8 @@ const String supabaseUrl = 'https://zxjswdmjhbeqnjaxlxbn.supabase.co';
 const String supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp4anN3ZG1qaGJlcW5qYXhseGJuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY4NzIyNDYsImV4cCI6MjA2MjQ0ODI0Nn0.80JpWfHlffBlpb-M8g0Fvxp677b0jLLO8bAzZ0ts3RI';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
 
   // Initialize Supabase
   final supabaseService = await SupabaseService.init(
@@ -48,6 +50,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // Remove the splash screen once the app is built
+    FlutterNativeSplash.remove();
+    
     final themeProvider = Provider.of<ThemeProvider>(context);
     
     return MaterialApp(
